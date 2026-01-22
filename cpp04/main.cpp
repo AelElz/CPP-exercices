08bae64e0c0abf470e87693721749feaef498f53
Ayoub elazhari <ayoub@Ayoubs-MacBook-Air-3.local>
2026-01-22 19:16:02 +0100

    cpp

---
+++ cpp04/main.cpp
@@ -0,0 +1,34 @@
+#include "Animal.hpp"
+#include "Dog.hpp"
+#include "Cat.hpp"
+#include "WrongAnimal.hpp"
+#include "WrongCat.hpp"
+
+int main()
+{
+	std::cout << "Correct Polymorphism" << std::endl;
+	const Animal* animal = new Animal();
+	const Animal* dog = new Dog();
+	const Animal* cat = new Cat();
+
+	std::cout << dog->getType() << std::endl;
+	std::cout << cat->getType() << std::endl;
+
+	cat->makeSound();
+	dog->makeSound();
+	animal->makeSound();
+
+	delete animal;
+	delete dog;
+	delete cat;
+
+	std::cout << "\nWrong Polymorphism" << std::endl;
+	const WrongAnimal* wrong = new WrongCat();
+
+	std::cout << wrong->getType() << std::endl;
+	wrong->makeSound(); // calls WrongAnimal sound
+
+	delete wrong;
+
+	return 0;
+}
